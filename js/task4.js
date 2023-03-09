28b78e0471a7995982fc5bb8ddbce81e2cdabfc3
naastichaak <nk01022008@gmail.com>
2023-03-09 12:19:26 +0200

    task8

---
+++ js/task4.js
@@ -0,0 +1,19 @@
+let firstNum = document.querySelector("#first-num").value;
+let secondNum = document.querySelector("#second-num").value;
+let result = document.querySelector("#result");
+
+let buttons = document.querySelector(".btn").value;
+let equal = document.querySelector("#equal");
+
+// const smth = () => {
+//     if (buttons == "+") {
+//         result = firstNum + secondNum;
+//     } else if (buttons == "*") {
+//         result = firstNum * secondNum;
+//     } else if (buttons == "-") {
+//         result = firstNum - secondNum;
+//     } else if (buttons == "/") {
+//         result = firstNum / secondNum;
+//     }
+// };
+
---
+++ js/task8.js
@@ -0,0 +1,20 @@
+let num1 = document.querySelector("#num1");
+let num2 = document.querySelector("#num2");
+let num3 = document.querySelector("#num3");
+
+let biggestNum = document.querySelector("#biggest-num");
+
+const biggestNumber = () => {
+    if (num1.value > num2.value && num1.value > num3.value) {
+        biggestNum.innerHTML = "Найбільше число, яке ви ввели - " + num1.value;
+        console.log(num1.value);
+    } else if (num2.value > num3.value && num2.value > num1.value) {
+        biggestNum.innerHTML = "Найбільше число, яке ви ввели - " + num2.value;
+        console.log(num2.value);
+    } else if (num3.value > num1.value && num3.value > num2.value) {
+        biggestNum.innerHTML = "Найбільше число, яке ви ввели - " + num3.value;
+        console.log(num3.value);
+    }
+};
+
+biggestNum.addEventListener("click", biggestNumber);
